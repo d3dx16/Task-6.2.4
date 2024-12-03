5e5af50850b6c3bf74c7da3a4f2bf94b0cb0921a
Yuriy.Boykov <d3dx16@gmail.com>
2024-12-03 22:04:06 +0700

    Как было

---
+++ src/Monster.java
@@ -0,0 +1,13 @@
+public class Monster{
+
+//    Конструктор
+    public Monster(String name, int damage){
+
+    }
+    public void growl(){
+       growl();
+    }
+    public void attack(){
+        attack();
+    }
+}
