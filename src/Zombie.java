b1c5e7646834b5c14619793a254094ad64cf4082
Yuriy.Boykov <d3dx16@gmail.com>
2024-12-11 20:48:01 +0700

    Всё правильно. Ура!

--- src/Zombie.java
+++ src/Zombie.java
@@ -1,20 +1,21 @@
 public class Zombie extends Monster {
 
     public Zombie(String name) {
-        super(name +" the Zombie" , 5);
-        System.out.println("Monster "+name+" the Zombie was created");
-    }
-
-    @Override
-    public void attack(){
-        super.attack();
-        System.out.println();
+        super(name + " the Zombie", 5);
     }
 
     @Override
     public void growl(){
         super.growl();
-        System.out.println(" growled");
+        System.out.print("Raaaauuughhhh");
+    }
+
+    @Override
+    public void attack(){
+        super.attack();
+        growl();
+        System.out.println();
     }
 
 }
+
